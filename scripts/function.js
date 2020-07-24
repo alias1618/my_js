55afe3092485e8d9c307d623a77d6fc30ba3d322
user <alias1617@gmail.com>
2020-07-24 13:32:00 +0800

    2020_0724_function_02

--- scripts/function.js
+++ scripts/function.js
@@ -37,7 +37,7 @@ console.log(sum(5, 6, 7));    // 6
 //console.log(sum(1, 2, 3, 4)); // 10
 */
 
-
+/*
 function func(a, b) {
     console.log(a + ': ' + arguments[0]);  // 10: 10
     console.log(b + ': ' + arguments[1]);  // 20: 20
@@ -52,3 +52,62 @@ function func(a, b) {
 }
 
 func(10, 20);
+*/
+
+/*
+function plus(a, b) {
+    if(arguments.length != 2) {
+        throw new Error('必須有兩個引數');
+    }
+    return a + b;
+}
+
+console.log(plus(10, 20)); // 30
+console.log(plus(10));     // Error: 必須有兩個引數
+
+*/
+
+/*
+function rangeClosed(startInclusive, endInclusive, step) {
+    var s = step || 1;
+    var numbers = [startInclusive];
+    for(var i = 0; numbers[i] < endInclusive; i++) {
+        numbers.push(numbers[i] + s);
+    }
+    return numbers;
+}
+
+rangeClosed(0, 10, 2).forEach(function(elem) { // 1 3 5
+    console.log(elem);
+});
+
+
+rangeClosed(1, 5).forEach(function(elem) {    // 1 2 3 4 5
+    console.log(elem);
+});
+*/
+
+
+function func(option) {
+    var opt = {
+        x : option.x || 10,
+        y : option.y || 20,
+        z : option.z || 30
+    };
+    console.log(opt.x);
+    console.log(opt.y);
+    console.log(opt.z);
+}
+
+func({x : 100}); // 100 20 30
+
+func({           // 100 200 30
+    x : 100, 
+    y : 200 
+});
+
+func({           // 100 200 300
+    x : 100, 
+    y : 200, 
+    z : 300
+});
---
+++ scripts/function_02.js
@@ -0,0 +1,30 @@
+
+/*
+function max(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+}
+
+var maximum = max;
+
+console.log(max(10, 20));      // 20
+console.log(maximum(10, 20));  // 20
+
+*/
+
+/*
+var max = function(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+};
+
+var maximum = max;
+
+console.log(max(10, 20));      // 20
+console.log(maximum(10, 20));  // 20
+*/
+
+var number = 10;        // Number literal
+var obj = {x : 10};     // Object literal
+var array = [1, 2, 3];  // Array literal
+var func = function() { // Function literal
+    // do something...
+};
