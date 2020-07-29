cdba5ec37b81f54190c272b677841f4cae093e75
user <alias1617@gmail.com>
2020-07-29 13:32:46 +0800

    2020_0729_function

--- scripts/function_02.js
+++ scripts/function_02.js
@@ -22,9 +22,42 @@ console.log(max(10, 20));      // 20
 console.log(maximum(10, 20));  // 20
 */
 
+/*
 var number = 10;        // Number literal
 var obj = {x : 10};     // Object literal
 var array = [1, 2, 3];  // Array literal
 var func = function() { // Function literal
     // do something...
 };
+
+
+*/
+
+/*
+var max = new Function('num1', 'num2', 
+    'return num1 > num2 ? num1 : num2'
+);
+
+var maximum = max;
+
+console.log(max(10, 20));      // 20
+console.log(maximum(10, 20));  // 20
+
+*/
+
+
+var printIt = function(elem) {
+    console.log(elem);
+};
+
+[1, 2, 3].forEach(printIt); // 1 2 3
+
+var naturalOrder = function(num1, num2) {
+    return num1 - num2;
+};
+
+[5, 1, 7, 3, 2].sort(naturalOrder)  // 1 2 3 5 7
+               .forEach(printIt);
+
+
+               
