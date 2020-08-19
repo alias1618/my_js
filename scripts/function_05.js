b10b4d6e38e6aa024f630276756d9ad02d975358
user <alias1617@gmail.com>
2020-08-19 13:43:06 +0800

    2020_0819_function_05_001

--- scripts/function_05.js
+++ scripts/function_05.js
@@ -1,3 +1,4 @@
+/*
 function Person(name, age) {
     this.name = name;
     this.age = age;
@@ -12,3 +13,38 @@ var p2 = new Person('Momor', 32);
 
 console.log(p1.toString());   // [Justin, 35]
 console.log(p2.toString());   // [Momor, 32]
+*/
+
+/*
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+Person.prototype.toString = function() {
+    return '[' + this.name + ', ' + this.age + ']';
+};
+
+var p = new Person('Justin', 35);
+
+console.log(Person.prototype === Object.getPrototypeOf(p));   // true
+
+*/
+
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+Person.prototype.toString = function() {
+    return '[' + this.name + ', ' + this.age + ']';
+};
+
+var p = {};
+p.__proto__ = Person.prototype;
+Person.call(p, 'Justin', 35);
+
+console.log(p.toString());         // [Justin,35]
+console.log(p instanceof Person);  // true
+
