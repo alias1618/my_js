bd0105a30017d3616938cb2ccbdc9ac0bd1c85e5
user <alias1617@gmail.com>
2020-08-21 13:35:55 +0800

    2020_0821_function_05_001

--- scripts/function_05.js
+++ scripts/function_05.js
@@ -31,7 +31,7 @@ console.log(Person.prototype === Object.getPrototypeOf(p));   // true
 
 */
 
-
+/*
 function Person(name, age) {
     this.name = name;
     this.age = age;
@@ -48,3 +48,52 @@ Person.call(p, 'Justin', 35);
 console.log(p.toString());         // [Justin,35]
 console.log(p instanceof Person);  // true
 
+*/
+
+/*
+function Some() {}
+Some.prototype.data = 10;
+
+var s = new Some();
+console.log(s.data);                 // 10
+
+s.data = 20;
+console.log(s.data);                 // 20
+console.log(Some.prototype.data);    // 10
+*/
+
+/*
+function Some() {}
+
+var s = new Some();
+console.log(s.data);       // undefined
+
+Some.prototype.data = 10;
+console.log(s.data);       // 10
+*/
+
+
+/*
+function Some() {}
+console.log(Some.prototype.constructor);  // [Function: Some]
+*/
+
+/*
+Object.prototype.xyz = 10;
+
+function Some() {}
+
+var s = new Some();
+console.log(s.xyz); // 10
+
+console.log(Object.getPrototypeOf(s) === Some.prototype);          // true
+
+var protoOfS = Object.getPrototypeOf(s);
+console.log(Object.getPrototypeOf(protoOfS) === Object.prototype); // true
+*/
+
+
+var arr = [];
+console.log(Array.prototype.isPrototypeOf(arr));              // true
+console.log(Function.prototype.isPrototypeOf(Array));         // true
+console.log(Object.prototype.isPrototypeOf(Array.prototype)); // true
