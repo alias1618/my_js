871cbfdd88db11e538693f355c3469e08370f366
user <alias1617@gmail.com>
2020-09-04 12:04:49 +0800

    2020_0904_function_06_001

--- scripts/function_06.js
+++ scripts/function_06.js
@@ -13,7 +13,7 @@ console.log(sportsCar.constructor);  // [Function: Car]
 
 */
 
-
+/*
 function Car() {
     Car.prototype.wheels = 4;
 }
@@ -26,3 +26,31 @@ function SportsCar() {
 var sportsCar = new SportsCar();
 console.log(sportsCar.doors);    // undefined
 console.log(sportsCar.wheels);   // undefined
+
+*/
+
+/*
+function Car() {}
+function SportsCar() {}
+SportsCar.prototype = new Car();
+
+var sportsCar = new SportsCar();
+console.log(sportsCar instanceof SportsCar);  // true
+console.log(sportsCar instanceof Car);        // true
+console.log(sportsCar instanceof Object);     // true
+
+*/
+
+var arrayLike = Object.create(Array.prototype, {
+    '0'    : {value : 10},
+    '1'    : {value : 20},
+    '2'    : {value : 30},
+    length : {value : 3}
+});
+
+console.log(arrayLike instanceof Array);  // true
+
+
+console.log(Array.prototype.isPrototypeOf([]));              // true
+console.log(Function.prototype.isPrototypeOf(Array));         // true
+console.log(Object.prototype.isPrototypeOf(Array.prototype)); // true
