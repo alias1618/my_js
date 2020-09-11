c1cb06dcdca8489f7b05873c68d00c1abd87aa8f
user <alias1617@gmail.com>
2020-09-11 12:13:18 +0800

    2020_0911_json_01_001

--- scripts/json_01.js
+++ scripts/json_01.js
@@ -21,6 +21,8 @@ var json =
 }
 */
 
+
+/*
 var obj = {
     name : 'Justin',
     age : 35,
@@ -35,4 +37,73 @@ console.log(json);
 //{"name":"Justin","age":35}
 console.log(JSON.stringify(obj, ['name', 'age']));
 
+*/
 
+/*
+var obj = {
+    name : 'Justin',
+    age : 35,
+    childs : [ {name : 'hamimi', age : 3} ]
+};
+
+console.log(JSON.stringify(obj, function(key, value) {
+    if(key === 'age'){
+        return undefined;
+    } 
+    return value;
+}));
+
+*/
+
+/*
+var obj = {
+    name : 'Justin',
+    age : 35,
+    childs : [ {name : 'hamimi', age : 3} ]
+};
+
+function replacer(key, value) {
+    return key === 'age' ? undefined : value;
+}
+
+console.log(JSON.stringify(obj, replacer, 2));
+
+*/
+
+/*
+
+var obj = {
+    name : 'Justin',
+    age : 35,
+    toJSON : function() {
+        return {
+            name : this.name.toUpperCase(), 
+            age  : this.age
+        };
+    }
+};
+
+// {"name":"JUSTIN","age":35}
+console.log(JSON.stringify(obj));
+
+*/
+
+
+/*
+var json = '{"name":"Justin","age":35,"childs":[{"name":"hamimi","age":3}]}';
+var obj = JSON.parse(json);
+
+console.log(obj.name); // Justin
+
+*/
+
+var json = '{"name":"Justin","age":35,"childs":[{"name":"hamimi","age":3}]}';
+
+var obj = JSON.parse(json, function(key, value) {   
+    if(key === 'age'){
+        return undefined;
+    } 
+    return value;
+});
+
+console.log(obj); // { name: 'Justin', childs: [ { name: 'hamimi' } ] }
